377716d140a98b30f7cb1eba1bd36ffe530b6600
laban254 <labanrotich6544@gmail.com>
2023-03-02 10:27:56 -0800

    is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,7 +36,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		max = right;
 	else
 		max = left;
-        return (max + 1);	
+	return (max + 1);
 }
 /**
  * binary_tree_balance - Calculate the balance factor of a tree
@@ -58,6 +58,7 @@ size_t binary_tree_balance(const binary_tree_t *tree)
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int left = 0, right = 0;
+
 	if (tree == NULL)
 		return (0);
 	left = binary_tree_is_perfect(tree->left);
