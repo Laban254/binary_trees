2ce418a9b52b77f8cb61457f9f352296ac130d3a
laban254 <labanrotich6544@gmail.com>
2023-03-02 18:38:43 +0300

    updated README

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -64,7 +64,7 @@ size_t binary_tree_balance(const binary_tree_t *tree)
 /**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: The binary tree
- * Return: 0  if tree is null
+ * Return: 1 if perfect else 0
  */
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
