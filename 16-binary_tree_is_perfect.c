8f470f3a3072d87400b1beed2bbd5f13cb010e2e
laban254 <labanrotich6544@gmail.com>
2023-03-02 20:55:50 +0300

    Is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -69,9 +69,14 @@ size_t binary_tree_balance(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int left = 0, right = 0;
 	if (tree == NULL)
 		return (0);
+	left = binary_tree_is_perfect(tree->left);
+	right = binary_tree_is_perfect(tree->right);
+	if (left != right)
+		return (0);
 	if (!binary_tree_balance(tree) && binary_tree_is_full(tree))
-		return (1)
+		return (1);
 	return (0);
-} k
+} 
